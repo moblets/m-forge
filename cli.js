c567c0809e405e8ba6c01d5b560cb79e18e7cee2
Leonardo "Alemax" Fonseca Ferreira <leualemax@gmail.com>
2016-05-09 10:59:33 -0300

    new functions and prepare now build scss

--- cli.js
+++ cli.js
@@ -14,20 +14,37 @@ cli.parse({
   target: ['t', 'target', 'string', "web"]
 });
 
+var sass = {
+  path: [process.cwd() + '/u-base/**/*.scss',
+        process.cwd() + '/u-core/**/*.scss',
+        process.cwd() + '/u-moblets/**/*.scss'],
+  location: process.cwd() + '/u-base/u-base.scss',
+  destination: process.cwd() + "/www/css/"
+};
+var js = {
+  path: [process.cwd() + '/u-base/**/*',
+         process.cwd() + '/u-core/**/*',
+         process.cwd() + '/u-moblets/**/*'],
+  location: [process.cwd() + "/u-moblets/u-moblets.js",
+             process.cwd() + "/u-core/u-core.js",
+             process.cwd() + "/u-base/u-base.js"],
+  destination: process.cwd() + "/www/bundles/"
+};
+var location = process.cwd() + "/www/";
+var destination = process.cwd() + "/www/bundles/";
+var manifest = process.cwd() + "/www/bundles/rev-manifest.json";
+
 cli.main(function(args, options) {
   var action = args[0];
   if (action === "prepare") {
-    var baseIonicBundles = [process.cwd() + "/u-moblets/u-moblets.js",
-                            process.cwd() + "/u-core/u-core.js",
-                            process.cwd() + "/u-base/u-base.js"];
-    var destination = process.cwd() + "/www/bundles/";
-    var manifest = process.cwd() + "/www/bundles/rev-manifest.json";
-    var index = process.cwd() + "/www/";
-    mForge.bundler.compile(baseIonicBundles, destination,
-                            options.min, options.rev)
-            .on("end", function() {
-              mForge.bundler.replace(manifest, index);
-            });
+    mForge.bundler.sass(sass.location, sass.destination, options.min)
+         .on('end', function() {
+           mForge.bundler.compile(js.location, destination,
+                                   options.min, options.rev)
+                   .on("end", function() {
+                     mForge.bundler.replace(manifest, location);
+                   });
+         });
   } else if (args[0] === "webserver" || args[0] === "mobile") {
     mForge.proprieties.change(process.cwd(), options.target, options.env,
             options.rev, options.app, function() {
@@ -37,24 +54,7 @@ cli.main(function(args, options) {
               }
             });
   } else if (args[0] === "develop") {
-    var sass  = {
-      path:[process.cwd() + '/u-base/**/*.scss',
-            process.cwd() + '/u-core/**/*.scss',
-            process.cwd() + '/u-moblets/**/*.scss'],
-      location:process.cwd() + '/u-base/u-base.scss',
-      destination:process.cwd() + "/www/css/"
-    };
-    var js  = {
-      path:[process.cwd() + '/u-base/**/*',
-            process.cwd() + '/u-core/**/*',
-            process.cwd() + '/u-moblets/**/*'],
-      location:[process.cwd() + "/u-moblets/u-moblets.js",
-                process.cwd() + "/u-core/u-core.js",
-                process.cwd() + "/u-base/u-base.js"],
-      destination:process.cwd() + "/www/bundles/"
-    }
-    var location = process.cwd() + "/www/";
-    mForge.develop.start(sass, js, location)
+    mForge.develop.start(sass, js, location);
   }
 });
 
